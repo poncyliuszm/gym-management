bdfec72429efbfb5c28bd9f2734b2feead3657d8
mateuszp <poncyliuszm@gmail.com>
2019-04-11 21:27:10 +0200

    menu - przy zmniejszeniu ekranu nie dzialalo rozwijanie menu. tlo przycisku wlaczonej zakladki w menu.

--- frontend/src/app/main-nav/main-nav.component.ts
+++ frontend/src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,8 +11,13 @@ import {AuthService} from "../services/auth.service";
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent {
+  @ViewChild('sidenav') sidenav:any;
 
   clickedItem;
+  toggleSidenav()
+  {
+    this.sidenav.toggle();
+  }
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
