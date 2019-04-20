86b9ba204c7298d191791afefb848c69667ee836
mateuszp <poncyliuszm@gmail.com>
2019-04-20 08:26:31 +0200

    Slownik pomiaru - fix. Literowka w ewidencji. Usuniecie zbendych importow.

--- backend/src/main/java/pl/nowosielski/repository/MeasurementTypeRepository.java
+++ backend/src/main/java/pl/nowosielski/repository/MeasurementTypeRepository.java
@@ -2,7 +2,6 @@ package pl.nowosielski.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.nowosielski.model.MeasurementType;
-import pl.nowosielski.model.TicketType;
 
 public interface MeasurementTypeRepository extends JpaRepository<MeasurementType, Integer> {
 }
